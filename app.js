a2b3434717f290f94631ef91451c07b2b3e11c1d
pratikvarshney <pratikvarshney@users.noreply.github.com>
2015-06-22 18:16:11 +0530

    ques print

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express    = require("express");
 var mysql      = require('mysql');
+var bodyParser = require('body-parser');
+
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -7,6 +9,9 @@ var connection = mysql.createConnection({
   database : 'db1'
 });
 var app = express();
+
+app.use(bodyParser());
+
 
 connection.connect(function(err){
 if(!err) {
@@ -22,17 +27,56 @@ res.sendFile(__dirname + '/public/forms/form.html');
 
 
 app.post("/",function(req,res){
-connection.query('SELECT * from mcqbank', function(err, rows, fields) {
-connection.end();
+
+var numQues = 10;
+var totalDuration = 60 * 60;
+
+connection.query('SELECT qid,duration from mcqbank order by duration', function(err, rows, fields) {
   if (!err)
-    console.log('The solution is: ', rows);
+    console.log('Query Done.');
   else
     console.log('Error while performing Query.');
 
-	var result = rows[0].question.toString();
-	res.send(rows);
+	//get random ques
+	var maxm = rows.length;
+	for(var i=0; i<numQues; i++)
+	{
+		var r = random(maxm);
+		//swap
+		var temp = rows[i];
+		rows[i] = rows[r];
+		rows[r] = temp;
+	}
+
+	//fix
+	//---
+	//fix end
+
+	//get ques
+	var ques = rows.splice(0, numQues);
+	var arr=[];
+	for(var i=0; i<ques.length; i++)
+	{
+		arr.push(Number(ques[i].duration));
+	}
+
+	connection.query('SELECT * from mcqbank where qid in (' + arr.toString() + ');', function(err, rows2, fields2) {
+	connection.end();
+	  if (!err)
+	    console.log('Question Query Done.');
+	  else
+	    console.log('Error while performing Question Query.');
+
+	  res.send(rows2);
+	});
+
 
   });
 });
+
+function random(maxval)
+{
+	return Math.floor(Math.random()*(maxval-1));
+}
 
 app.listen(3000);
